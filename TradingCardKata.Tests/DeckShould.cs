7e0649a3fa5dc062744c5a33484fff0620c0dae3
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2019-03-24 19:05:34 +0000

    draw_a_random_card

--- TradingCardKata.Tests/DeckShould.cs
+++ TradingCardKata.Tests/DeckShould.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace TradingCardKata.Tests {
     public class DeckShould {
+        private Deck deck;
+        private Random random;
+
+        [SetUp]
+        public void SetUp() {
+            random = Substitute.For<Random>();
+            deck = new Deck(random);
+        }
 
         [Test]
         public void have_20_cards() {
-            var deck = new Deck();
             deck.Cards.Should().HaveCount(20);
         }
 
@@ -22,16 +31,28 @@ namespace TradingCardKata.Tests {
         [TestCase(7, 1)]
         [TestCase(8, 1)]
         public void have_cards_with_mana_cost(int manaCost, int amountOfCards) {
-            var deck = new Deck();
             var cardsWithManaCost = deck.Cards.Where(x => x.ManaCost == manaCost);
             cardsWithManaCost.Should().HaveCount(amountOfCards);
+        }
+        
+        [Test]
+        public void draw_a_random_card() {
+            var cardNumber = 18;
+            random.Next().Returns(cardNumber);
+            var expectedCard = deck.Cards[cardNumber];
+
+            var card = deck.DrawCard();
+
+            expectedCard.Should().Be(card);
         }
     }
 
     public class Deck {
+        private readonly Random random;
         public List<Card> Cards { get; }
 
-        public Deck() {
+        public Deck(Random random) {
+            this.random = random;
             Cards = new List<Card>(Enumerable.Repeat(new Card(0), 2))
                 .Concat(Enumerable.Repeat(new Card(1), 2))
                 .Concat(Enumerable.Repeat(new Card(2), 3))
@@ -42,6 +63,11 @@ namespace TradingCardKata.Tests {
                 .Concat(Enumerable.Repeat(new Card(7), 1))
                 .Concat(Enumerable.Repeat(new Card(8), 1))
                 .ToList();
+        }
+
+        public Card DrawCard() {
+            var cardIndex = random.Next();
+            return Cards[cardIndex];
         }
     }
 
